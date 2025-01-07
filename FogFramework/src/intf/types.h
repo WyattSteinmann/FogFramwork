0b57d857035cf241342daa40ffb3511d446d3405
Doggo <157554405+WyattSteinmann@users.noreply.github.com>
2025-01-07 12:52:17 -0600

    Add files via upload

---
+++ FogFramework/src/intf/types.h
@@ -0,0 +1,22 @@
+// Not My Code.
+
+#pragma once
+
+typedef signed char int8;
+typedef unsigned char uint8;
+
+typedef signed short int16;
+typedef unsigned short uint16;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+
+typedef signed long long int64;
+typedef unsigned long long uint64;
+
+typedef char *string;
+
+typedef uint8_t bool;
+
+#define false 0
+#define true 1
